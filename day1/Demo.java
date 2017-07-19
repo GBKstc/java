e3d498141cbc1012e8ec54289aa5c077a0d9cc99
Administrator <gbk@email>
2017-07-19 19:39:33 +0800

    add

---
+++ day1/Demo.java
@@ -0,0 +1,14 @@
+public class Demo
+		{
+			public static void main(String[] args) 
+			{
+				System.out.println(15);
+				System.out.println(0B1111);
+				System.out.println(0xF);
+				System.out.println(017);
+				
+				System.out.println(true);
+				System.out.println(false);
+	
+			}
+		}
---
+++ day2/Variable.java
@@ -0,0 +1,14 @@
+public class Variable{
+	public static void main(String[] args){
+		byte b = 100;
+		short s = 200;
+		int i = 50000;
+		long l = 500000000l;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		String title = "¼Û¸ñ±í";
+		System.out.println(title);
+	}
+}
